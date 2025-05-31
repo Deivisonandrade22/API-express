1568fa8687088dc3651e61fc17ab4b5cc94d3aac
Deivison <deivisonv.andrade@gmail.com>
2025-05-31 19:55:54 -0300

    outra parte

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 
+
 const app = express();
- app.length("/", (req,res) ={
-    res.status(200).send("Curso de Node.js"):
- })
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node.js");
+});
+ export default app;
